897e0ad9fa57dfdec6a9418c33543487934c9932
Gavin Zhao <git@gzgz.dev>
2022-09-27 20:46:03 -0400

    infra: Update pkgs list and improve utils.sh
    Signed-off-by: Gavin Zhao <git@gzgz.dev>

--- utils.sh
+++ utils.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
+set -eo pipefail
+
 upload() {
-	fd . -I -e stone $1 --exec aliyun oss cp {} oss://venom-testing/{}
+	echo "The following packages will get uploaded:"
+	fd . -u -e stone "$1"
+	fd . -u -e stone "$1" --exec aliyun oss cp {} oss://venom-testing/{}
 }
 
 delete() {
-	aliyun oss rm -r oss://venom-testing/$1
+	echo "Deleting packages in oss://venom-testing/${1}"
+	aliyun oss rm -r oss://venom-testing/"$1"
 }
 
 check() {
@@ -13,13 +18,42 @@ check() {
 }
 
 build() {
+	# boulder's return code is STILL 0 even if build fails!
 	sudo boulder build -p venom-x86_64
 }
 
 find-pkg() {
-	fd -I -e stone $1
+	fd -u -e stone "$1"
+}
+
+build-pkg() {
+	pushd "$1" || exit 1
+	build
+	popd || exit 1
+
+	upload "$1"
 }
 
 clean() {
-	git clean -f -x -e .env -e "*tfstate*"
+	echo "Cleaning $1"
+	git clean -f -x -e .env -e "*tfstate*" "$1"
 }
+
+rebuild() {
+	if [[ ! -f "${1}/stone.yml" ]]; then
+		echo "No stone.yml found in ${1}!"
+		exit 1
+	fi
+
+	clean "$1"
+
+	echo "Rebuilding package ${1}"
+	pushd "$1" || exit 1
+	build
+	popd || exit 1
+
+	delete "$1"
+	upload "$1"
+}
+
+$1 $2
